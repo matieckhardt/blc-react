8b1a771b3edc8c6b8b9fc3c985ed77b68a5a2ffc
matieckhardt <matias.eckhardt@gmail.com>
2023-11-03 16:08:03 -0300

    remove mantine carousel

--- app/routes/Clients.tsx
+++ app/routes/Clients.tsx
@@ -1,44 +1,47 @@
+import React, { useCallback, useEffect, useState } from "react";
+import useEmblaCarousel from "embla-carousel-react";
 import { Title, Text, Image } from "@mantine/core";
 import classes from "../css/Clients.module.css";
-import { Carousel } from "@mantine/carousel";
-
-import "@mantine/carousel/styles.css";
 
 export default function Clients() {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+
+  useEffect(() => {
+    if (emblaApi) {
+      // Embla API is ready to use
+    }
+  }, [emblaApi]);
+
   return (
     <div className={classes.main} id="clients">
-      <Title ta="center" c="#0168B6" p={2} pt={30} size={55}>
+      <Title align="center" color="#0168B6" p={2} pt={30} size={55}>
         Nuestros Clientes
       </Title>
 
-      <Text ta="center" c="#0168B6" p={2} size="1.3rem">
+      <Text align="center" color="#0168B6" p={2} size="1.3rem">
         Desde hace mas de 30 años, hemos desarrolado soluciones para mas de xx
         empresas
       </Text>
 
       <div className={classes.clientsCar}>
-        <div>
-          <Carousel
-            slideSize="100%"
-            withIndicators
-            p={100}
-            withControls={false}
-            classNames={classes}
-          >
-            <Carousel.Slide>
-              <Image src="/clients/1.png"></Image>{" "}
-            </Carousel.Slide>
-            <Carousel.Slide>
-              <Image src="/clients/2.png"></Image>{" "}
-            </Carousel.Slide>
-            <Carousel.Slide>
-              <Image src="/clients/3.png"></Image>{" "}
-            </Carousel.Slide>
-            <Carousel.Slide>
-              <Image src="/clients/4.png"></Image>{" "}
-            </Carousel.Slide>
-            {/* ...other slides */}
-          </Carousel>
+        <div className={classes.embla}>
+          <div className={classes.embla__viewport} ref={emblaRef}>
+            <div className={classes.embla__container}>
+              <div className={classes.embla__slide}>
+                <Image src="/clients/1.png" />
+              </div>
+              <div className={classes.embla__slide}>
+                <Image src="/clients/2.png" />
+              </div>
+              <div className={classes.embla__slide}>
+                <Image src="/clients/3.png" />
+              </div>
+              <div className={classes.embla__slide}>
+                <Image src="/clients/4.png" />
+              </div>
+              {/* ...other slides */}
+            </div>
+          </div>
         </div>
       </div>
     </div>
